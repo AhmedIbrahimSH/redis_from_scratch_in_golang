5e528d35d755d2f443b47d366c9ef9be4937f63e
AhmedIbrahimSH <ahmedibrahim132002@gmail.com>
2025-11-07 23:54:55 +0200

    feat: finished the main file, which parses redis commands in RESP protocol feature, char by char each line for now

--- main.go
+++ main.go
@@ -3,11 +3,85 @@
 
 package main 
 
-import "fmt"
-
+import (
+  
+  "fmt"
+  "io"
+  "net"
+  "bufio"
+  "strings"
+  "os"
+  "strconv"
+) 
 
 
 func main() {
 
-  fmt.Println("hossam")
+  fmt.Println("Starting to build redis from scratch as we possibly can")
+  fmt.Println("lets go")
+  
+  l , err := net.Listen("tcp", ":6379")
+
+  if err != nil {
+        fmt.Println(err)
+	return
+  }
+  
+  conn , err := l.Accept()
+
+  if err != nil {
+        fmt.Println(err)
+	return
+  }
+  
+  defer conn.Close()
+
+  // infinite for loop to listen to commands 
+  
+
+  for {
+  
+    buf := make([]byte , 1024)
+
+    // read message from cmd
+
+    _, err = conn.Read(buf)
+    
+    if err != nil {
+	
+      if err == io.EOF {
+	  
+	break
+      }
+
+      fmt.Println("Error reading from client ", err.Error())
+      os.Exit(1)
+    }
+
+    conn.Write([]byte ("+OK\r\n"))
+    
+    input := "$5\r\nAhmed\r\n"
+
+    reader := bufio.NewReader(strings.NewReader(input))
+
+    b, _ := reader.ReadByte()
+
+    if b != '$' {
+          fmt.Println("Invalid type, expecting bulk strings only")
+	      os.Exit(1)
+    }
+    size, _ := reader.ReadByte()
+
+    strSize, _ := strconv.ParseInt(string(size), 10, 64)
+
+    reader.ReadByte()
+    reader.ReadByte()
+
+    name := make([]byte, strSize)
+    reader.Read(name)
+
+    fmt.Println(string(name))
+    
+
+  }
 }
